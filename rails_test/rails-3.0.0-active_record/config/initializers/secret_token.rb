dce4121e5b21868434d0e135941397ef8498785b
Mike Dalessio <mike@csa.net>
2010-10-26 00:41:45 -0400

    updated tests to rails 3.0.0 (from .beta4)

---
+++ rails_test/rails-3.0.0-active_record/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails300ActiveRecord::Application.config.secret_token = '2c3184afc7f9cc6fac5306cae59c67d9834b5bb910917b20d4ee4cf2ff80ebeaa744edaf8f80195d97d20cc0845c3e00a18c46d89e137701362262be99ce501f'
---
+++ rails_test/rails-3.0.0-xss_foliate/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails300XssFoliate::Application.config.secret_token = 'a6ae55443efa9bafba24c5adfb45fb9b6674af183f60367e0d964991526e9eae47f43f88877ed659a8e2abb89008cb2b901dacc092d6cbf62e923cca9355a48e'
