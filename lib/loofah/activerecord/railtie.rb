042e5da873657f2c4f5698f5573b8163d42e780f
Mike Dalessio <mike.dalessio@gmail.com>
2014-08-22 17:22:54 -0400

    Adding support for Rails 4.
    Resolves #4.

--- lib/loofah/activerecord/railtie.rb
+++ lib/loofah/activerecord/railtie.rb
@@ -2,7 +2,12 @@ require 'rails'
 module Loofah::ActiveRecord
   class Railtie < Rails::Railtie
     initializer "loofah-activerecord.initialize" do |app|
-      activerecord_railtie = app.railties.all.select {|railtie| railtie.class.to_s == "ActiveRecord::Railtie" }
+      railtie_collection = if app.railties.respond_to?(:all) # Rails::VERSION::MAJOR == 3
+                             app.railties.all
+                           else # Rails::VERSION::MAJOR == 4
+                             app.railties
+                           end
+      activerecord_railtie = railtie_collection.select {|railtie| railtie.class.to_s == "ActiveRecord::Railtie" }
       if activerecord_railtie
         require 'loofah/activerecord/active_record'
         require 'loofah/activerecord/xss_foliate'
