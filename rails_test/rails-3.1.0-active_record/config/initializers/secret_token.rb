91dcbda1983d29a2effb1fde18272621b87ff93d
Mike Dalessio <mike.dalessio@gmail.com>
2016-11-22 07:33:29 -0500

    removing support for rails 1-3
    and thus also ruby 1.8.7-1.9.3.

--- rails_test/rails-3.1.0-active_record/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Rails310ActiveRecord::Application.config.secret_token = '88aa7a75807517ba30b9bd3ed8050177592b8701d78c05818da78e82310ca993fc28f4637e26188e1ad208ff3286bd382eb4e7a326b240ef7bb7ddd6409abfcf'
--- rails_test/rails-3.2.0-xss_foliate/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Rails320XssFoliate::Application.config.secret_token = '20be49605179a5c7e33c5d77c5b193fb94426ce528d608ab7dc47db25482e625d2e5dc53dae40c55be3c22bfd49b2f11d3296a11ede1ee49e2dac1528ca1b3d2'
