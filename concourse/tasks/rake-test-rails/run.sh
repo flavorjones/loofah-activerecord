67034dbe64c68cb27875bdebc1994bb63316576d
Mike Dalessio <mike.dalessio@gmail.com>
2020-11-26 11:54:04 -0500

    ensure we have a min version of bundler

--- concourse/tasks/rake-test-rails/run.sh
+++ concourse/tasks/rake-test-rails/run.sh
@@ -7,6 +7,7 @@ pushd loofah-activerecord
   apt-get update
   apt-get install -y rsync
 
+  gem install bundler -v 2.1.4
   bundle install
   bundle exec rake test:rails:$TEST_RAILS_VERSION-xss_foliate
   bundle exec rake test:rails:$TEST_RAILS_VERSION-active_record
