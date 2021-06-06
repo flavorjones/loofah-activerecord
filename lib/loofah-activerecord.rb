304b6a192342523263f5882ddb7fa3f38b9d93b1
Mike Dalessio <mike.dalessio@gmail.com>
2021-06-06 13:22:25 -0400

    use require_relative

--- lib/loofah-activerecord.rb
+++ lib/loofah-activerecord.rb
@@ -1,5 +1,3 @@
-$LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__))) unless $LOAD_PATH.include?(File.expand_path(File.dirname(__FILE__)))
-
 require 'loofah'
 
 module Loofah::ActiveRecord
@@ -7,13 +5,13 @@ module Loofah::ActiveRecord
 end
 
 if defined?(Rails) && defined?(Rails::Railtie)
-  require 'loofah/activerecord/railtie'
+  require_relative 'loofah/activerecord/railtie'
 elsif defined? Rails.configuration and Rails.configuration.frameworks.include?([:active_record]) # >= 2.1
   Rails.configuration.after_initialize do
-    require 'loofah/activerecord/active_record'
-    require 'loofah/activerecord/xss_foliate'
+    require_relative 'loofah/activerecord/active_record'
+    require_relative 'loofah/activerecord/xss_foliate'
   end
 elsif defined? ActiveRecord::Base # <= 2.0
-  require 'loofah/activerecord/active_record'
-  require 'loofah/activerecord/xss_foliate'
+  require_relative 'loofah/activerecord/active_record'
+  require_relative 'loofah/activerecord/xss_foliate'
 end
